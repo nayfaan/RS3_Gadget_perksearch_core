0d9fcdc322fb54e1ab4f53dbb93759145529686e
nayfaan <luknnn129@gmail.com>
2023-10-18 07:34:42 -0700

    v.0.0.2. - Fixed tqdm - Fixed permutator

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tqdm import tqdm
 import itertools
-import io
 
 comps = {
     "normal": {
@@ -228,28 +227,19 @@ comps = {
     },
 }
 
-class tqdmIOWrapper(io.TextIOBase):
-    def __init__(self, file, callback):
-        self.file = file
-        self.callback = callback
-    def write(self,string):
-        buf = self.file.write(string)
-        print(buf)
-        if buf:
-            self.callback(buf)
-        return buf
-
 def write2file(arr, filename):
     with open("./output/" + filename + ".js", "w") as f:
         f.write("var " + filename + " = ")
         arr = str(arr)
+        n = len(arr)
+        k = max(1000, int(n/100))
         with tqdm(
-            total=len(arr), desc="Printing: " + filename, mininterval=1
+            total=n, desc="Printing: " + filename, mininterval=1
         ) as pbar:
-            # f.write(arr)
-            wrapper = tqdmIOWrapper(f, pbar.update)
-            wrapper.write(arr)
-            pbar.update()
+            for i in range(0, n, k):
+                chunk = arr[i : i+k]
+                f.write(chunk)
+                pbar.update(len(chunk))
 
     print("Completed: " + filename)
 
@@ -262,9 +252,9 @@ def comps_permutator(gizmo, type):
     elif type == "ancient":
         slots = 9
         iterable = iterable + comps["ancient"][gizmo]
-    elif type == "test":
-        slots = 13
-        iterable = comps["ancient"][gizmo]
+    # elif type == "test":
+    #     slots = 13
+    #     iterable = comps["ancient"][gizmo]
 
     print()
     result = []
@@ -283,28 +273,28 @@ def comps_permutator(gizmo, type):
 def run():
     perm = {"tool": {}, "weapon": {}, "armour": {}}
 
-    test = comps_permutator("armour", "test")
-    write2file(test, "test")
+    # test = comps_permutator("armour", "test")
+    # write2file(test, "test")
 
-    # perm["tool"]["normal"] = comps_permutator("tool", "normal")
-    # write2file(perm["tool"]["normal"],"tool_normal")
+    perm["tool"]["normal"] = comps_permutator("tool", "normal")
+    write2file(perm["tool"]["normal"],"tool_normal")
 
-    # perm["tool"]["ancient"] = comps_permutator("tool", "ancient")
-    # write2file(perm["tool"]["ancient"],"tool_ancient")
+    perm["tool"]["ancient"] = comps_permutator("tool", "ancient")
+    write2file(perm["tool"]["ancient"],"tool_ancient")
 
-    # perm["weapon"]["normal"] = comps_permutator("weapon", "normal")
-    # write2file(perm["weapon"]["normal"],"weapon_normal")
+    perm["weapon"]["normal"] = comps_permutator("weapon", "normal")
+    write2file(perm["weapon"]["normal"],"weapon_normal")
 
-    # perm["weapon"]["ancient"] = comps_permutator("weapon", "ancient")
-    # write2file(perm["weapon"]["ancient"],"weapon_ancient")
+    perm["weapon"]["ancient"] = comps_permutator("weapon", "ancient")
+    write2file(perm["weapon"]["ancient"],"weapon_ancient")
 
-    # perm["armour"]["normal"] = comps_permutator("armour", "normal")
-    # write2file(perm["armour"]["normal"],"armour_normal")
+    perm["armour"]["normal"] = comps_permutator("armour", "normal")
+    write2file(perm["armour"]["normal"],"armour_normal")
 
-    # perm["armour"]["ancient"] = comps_permutator("armour", "ancient")
-    # write2file(perm["armour"]["ancient"],"armour_ancient")
+    perm["armour"]["ancient"] = comps_permutator("armour", "ancient")
+    write2file(perm["armour"]["ancient"],"armour_ancient")
 
-    # write2file(perm,"perm")
+    write2file(perm,"perm")
 
 if __name__ == "__main__":
     run()
