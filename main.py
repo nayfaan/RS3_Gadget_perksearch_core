152ee7f6b6c2574dc1525e9cccf851c8ce7e194d
nayfaan <luknnn129@gmail.com>
2023-10-18 22:32:46 -0700

    v.0.1.1. - Changed from permutation to combination - more NumPy for .tofile()

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from tqdm import tqdm
 import itertools
 import numpy as np
+import math
 
 from services.comps import comps
 from services.comp_list import comp_list
 
+
 def reverseDict(_dict, _val):
     return list(_dict.keys())[list(_dict.values()).index(_val)]
 
+
+def combLen(n, r):
+    return math.factorial(n + r - 1) / (math.factorial(r) * math.factorial(n - 1))
+
+
 def write2file(arr, filename):
+    _arr = np.empty(len(arr), dtype=object)
+    _arr[:] = arr
+
+    n = len(_arr)
+    k = min(max(100, int(n / 100)), n)
+
     with open("./output/" + filename + ".js", "w") as f:
-        f.write("var " + filename + " = ")
-        arr = str(arr)
-        n = len(arr)
-        k = max(1000, int(n/100))
-        with tqdm(
-            total=n, desc="Print: " + filename, mininterval=1
-        ) as pbar:
+        f.write("var " + filename + " = [")
+        
+        with tqdm(total=n, desc="Print: " + filename, mininterval=1) as pbar:
             for i in range(0, n, k):
-                chunk = arr[i : i+k]
-                f.write(chunk)
+                chunk = _arr[i : i + k]
+                chunk.tofile(f, sep=', ')
                 pbar.update(len(chunk))
+
+        f.write("]")
 
     print("Completed: " + filename)
 
@@ -33,18 +44,18 @@ def comps_permutator(gizmo, type):
     elif type == "ancient":
         slots = 9
         iterable = np.concatenate([iterable, comps["ancient"][gizmo]])
-    elif type == "test":
-        slots = 4
-        iterable = np.concatenate([iterable, comps["ancient"][gizmo]])
+    # elif type == "test":
+    #     slots = 5
+    #     iterable = np.concatenate([iterable, comps["ancient"][gizmo]])
 
     print()
     result = []
     with tqdm(
-        total=pow(len(iterable), slots),
+        total=combLen(len(iterable), slots),
         desc="Ite: " + type + " " + gizmo,
         mininterval=1,
     ) as pbar:
-        for p in itertools.product(tqdm(iterable), repeat=slots):
+        for p in itertools.combinations_with_replacement(tqdm(iterable), slots):
             result.append(p)
             pbar.update()
 
@@ -58,26 +69,26 @@ def run():
     # write2file(test, "test")
 
     perm["tool"]["normal"] = comps_permutator("tool", "normal")
-    write2file(perm["tool"]["normal"],"tool_normal")
+    write2file(perm["tool"]["normal"], "tool_normal")
 
     perm["tool"]["ancient"] = comps_permutator("tool", "ancient")
-    write2file(perm["tool"]["ancient"],"tool_ancient")
+    write2file(perm["tool"]["ancient"], "tool_ancient")
 
     perm["weapon"]["normal"] = comps_permutator("weapon", "normal")
-    write2file(perm["weapon"]["normal"],"weapon_normal")
+    write2file(perm["weapon"]["normal"], "weapon_normal")
 
     perm["weapon"]["ancient"] = comps_permutator("weapon", "ancient")
-    write2file(perm["weapon"]["ancient"],"weapon_ancient")
+    write2file(perm["weapon"]["ancient"], "weapon_ancient")
 
     perm["armour"]["normal"] = comps_permutator("armour", "normal")
-    write2file(perm["armour"]["normal"],"armour_normal")
+    write2file(perm["armour"]["normal"], "armour_normal")
 
     perm["armour"]["ancient"] = comps_permutator("armour", "ancient")
-    write2file(perm["armour"]["ancient"],"armour_ancient")
+    write2file(perm["armour"]["ancient"], "armour_ancient")
 
-    # write2file(perm,"perm")
+    write2file(perm, "perm")
+
 
 if __name__ == "__main__":
     run()
     print()
-
